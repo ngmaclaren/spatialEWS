1a64e4cd69032b84b1ac2dc96c2b3e5d40d16043
ngmaclaren <neil.g.maclaren@gmail.com>
2024-05-10 10:53:22 -0400

    Change sim prep style, add easy way to check sims

---
+++ check-simresults.R
@@ -0,0 +1,40 @@
+library(igraph)
+library(localsolver)
+
+networks <- readRDS("./data/networks.rds")
+
+simdir <- "./data/sims/"
+simfiles <- list.files(simdir)
+simdata <- lapply(paste0(simdir, simfiles), function(simfile) readRDS(simfile))
+
+dyns <- sapply(simdata, attr, "model")
+nets <- sapply(simdata, attr, "network")
+bparams <- sapply(simdata, attr, "bparam")
+directions <- sapply(simdata, attr, "direction")
+cparam.vals <- lapply(simdata, attr, "bparam.vals")
+As <- lapply(simdata, function(dat) attr(dat, "params")$A)
+
+dfs <- lapply(
+    simdata,
+    function(sim) {
+        attribs <- attributes(sim)
+        df <- sim[[1]]
+        attributes(df) <- c(attributes(df), attribs)
+        df
+    }
+)
+
+dfnames <- mapply(
+    function(dyn, net, bp, dir) paste(dyn, net, bp, dir, sep = "-"),
+    dyns, nets, bparams, directions,
+    USE.NAMES = FALSE
+)
+names(dfs) <- dfnames
+
+conds <- as.data.frame(cbind(dyns, bparams, directions, nets))
+plotorder <- with(conds, order(dyns, bparams, directions, nets))
+
+pdf("./img/checksims.pdf")
+mapply(function(df, cparam.val, main) bifplot(df, cparam.val, lwd = 0.5, col = 1, main = main),
+       dfs[plotorder], cparam.vals[plotorder], dfnames[plotorder])
+dev.off()
