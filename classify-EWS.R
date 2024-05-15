9ee4261efb80ad4a9193b2f82899189de64c8747
ngmaclaren <neil.g.maclaren@gmail.com>
2024-05-15 06:54:07 -0400

    Update

--- classify-EWS.R
+++ classify-EWS.R
@@ -9,6 +9,9 @@ library(igraph)
 library(localsolver)
 source("calc-functions.R")
 palette("Okabe-Ito")
+## with(list(n = length(palette())), plot(seq(n), rep(1, n), col = seq(n), cex = 5, pch = 16))
+## dev.new(width = 14, height = 3); plot(0:24, rep(1, 25), pch = 0:24, cex = 5, col = 1, bg = 2)
+save_plots <- FALSE
 
 networks <- readRDS("./data/networks.rds")
 
@@ -41,7 +44,7 @@ dfnames <- mapply(
 names(dfs) <- dfnames
 
 conds <- as.data.frame(cbind(dyns, bparams, directions, nets))
-plotorder <- with(conds, order(dyns, bparams, directions, nets))
+plotorder <- with(conds, order(dyns, bparams, directions, nets)) # makes it alphabetical. Should specify. Want the double-well type and the transcritical type showing up together.
 
 u.down <- which(grepl("-u-", dfnames, fixed = TRUE) & grepl("-down", dfnames, fixed = TRUE))
 u.up <- which(grepl("-u-", dfnames, fixed = TRUE) & grepl("-up", dfnames, fixed = TRUE))
@@ -86,13 +89,21 @@ get_samples <- function(df, which = c("near", "far"), n = 3) {
     )
 }
 
-get_tau <- function(df, EWS) {
+get_tau <- function(df, EWS, adjust.sign = FALSE) {
     idx <- get_idx(df)
 
     cparam <- attr(df, "bparam.vals")[idx]
     ews <- EWS[idx]
 
-    cor(cparam, ews, method = "kendall")
+    tau <- cor(cparam, ews, method = "kendall")
+
+    if(adjust.sign) {
+        if(attr(df, "direction") == "down") {
+            tau <- -tau
+        }
+    }
+
+    return(tau)
 }
 
 get_slope <- function(df, EWS, which = c("near", "far"), n = 3, return.model = FALSE) {
@@ -111,13 +122,80 @@ get_slope <- function(df, EWS, which = c("near", "far"), n = 3, return.model = F
     }
 }
 
-                                        # test
+## So, measure the "sign adjusted" tau, where for the down direction we flip the sign of tau so that a positive large tau is always a good thing regardless of the up or down direction.
+taus <- list(
+    moranI = mapply(get_tau, dfs, moranIs, TRUE),
+    ssd = mapply(get_tau, dfs, ssds, TRUE)
+)
 slopes <- data.frame(
     ssd.far = mapply(get_slope, dfs, ssds, which = "far", n = 3),
     ssd.near = mapply(get_slope, dfs, ssds, which = "near", n = 3),
     moran.far = mapply(get_slope, dfs, moranIs, which = "far", n = 3),
     moran.near = mapply(get_slope, dfs, moranIs, which = "near", n = 3)
 )
+
+plotdata <- data.frame(
+    tau.I = taus$moranI, # these are 'sign-adjusted'
+    tau.sd = taus$ssd,
+    slope.far.I = slopes$moran.far,
+    slope.near.I = slopes$moran.near,
+    slope.far.sd = slopes$ssd.far,
+    slope.near.sd = slopes$ssd.near,
+    dynamics = dyns,
+    network = nets,
+    cparam = bparams,
+    direction = directions,
+    row.names = seq_along(dfs)
+)
+plotdata$dynamics <- factor(plotdata$dynamics, levels = c("doublewell", "mutualistic", "SIS", "genereg"))
+plotdata$network <- factor(plotdata$network)
+plotdata$cparam <- factor(plotdata$cparam)
+plotdata$direction <- factor(plotdata$direction)
+
+## then maybe a strip plot by qualitative var
+## Color by EWS  #### START HERE: separate more. Within a dynamics, four cols: I D, SD D, I u, SD u
+ht <- 5; wd <- 12
+if(save_plots) {
+    pdf("./img/tauplot.pdf", height = ht, width = wd)
+} else {
+    dev.new(height = ht, width = wd)
+}
+labelsize <- 1.75
+ptcex <- 2
+par(mfrow = c(1, 2), mar = c(4, 10, 0.5, 0.5))
+with(subset(plotdata, direction == "down"), {
+    xlim <- c(-1, 1)
+    ylim <- rev(c(0.5, 4.5))
+    pchs <- with(list(x = as.numeric(cparam)), ifelse(x == 1, x, 4))
+    ##adj <- as.numeric(cparam)/4
+    plot(NULL, xlim = xlim, ylim = ylim, axes = FALSE, xlab = "", ylab = "")
+    abline(v = 0)
+    axis(2, at = 1:4, labels = levels(dynamics), cex.axis = labelsize, las = 2)
+    axis(1, cex.axis = labelsize)
+    title(xlab = "Sign-adjusted tau", cex.lab = labelsize)
+    mtext("(a)", line = -1.5, adj = 0.01, cex = labelsize)
+    box()
+    points(tau.I, jitter(as.numeric(dynamics) - 0.2, amount = 0.1), pch = pchs, col = 2, cex = ptcex)
+    points(tau.sd, jitter(as.numeric(dynamics) + 0.2, amount = 0.1), pch = pchs, col = 3, cex = ptcex)
+})
+with(subset(plotdata, direction == "up"), {
+    xlim <- c(-1, 1)
+    ylim <- rev(c(0.5, 4.5))
+    pchs <- with(list(x = as.numeric(cparam)), ifelse(x == 1, x, 4))
+    plot(NULL, xlim = xlim, ylim = ylim, axes = FALSE, xlab = "", ylab = "")
+    abline(v = 0)
+    axis(2, at = 1:4, labels = levels(dynamics), cex.axis = labelsize, las = 2)
+    axis(1, cex.axis = labelsize)
+    title(xlab = "Sign-adjusted tau", cex.lab = labelsize)
+    mtext("(b)", line = -1.5, adj = 0.01, cex = labelsize)
+    box()
+    points(tau.I, jitter(as.numeric(dynamics) - 0.2, amount = 0.1), pch = pchs, col = 2, cex = ptcex)
+    points(tau.sd, jitter(as.numeric(dynamics) + 0.2, amount = 0.1), pch = pchs, col = 3, cex = ptcex)
+    legend("bottomright", bty = "n", ncol = 1, col = c(2:3, 9, 9), pch = c(1, 1, 1, 4),
+           cex = 0.75*labelsize, pt.cex = 0.75*ptcex,
+           legend = c("Moran's I", "SSD", "D", "u"))
+})
+if(save_plots) dev.off()
 
                                         # I think we don't want an absolute value threshold: no way to know in
                                         # advance what scale the slope differences will be on.
@@ -218,5 +296,7 @@ results <- data.frame(
 ## addmargins(table(results$direction, results$result, results$EWS))[, , 1:2]
 ## table(results$dynamics, results$direction,  results$result, results$EWS)
 
+print("Moran's I")
 with(list(df = subset(results, EWS == "moranI")), table(df$dynamics, df$bparam, df$result, df$direction))
+print("Spatial standard deviation")
 with(list(df = subset(results, EWS == "ssd")), table(df$dynamics, df$bparam, df$result, df$direction))
