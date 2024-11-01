a3b99ee80a347bf71659f69243d10fa311d85316
ngmaclaren <neil.g.maclaren@gmail.com>
2024-11-01 16:57:05 -0400

    Add CV and 2nd moment to EWS-data

--- prepare-EWS-data.R
+++ prepare-EWS-data.R
@@ -2,6 +2,7 @@ library(parallel)
 ncores <- detectCores()-1
 library(igraph)
 library(sfsmisc)
+library(moments)
 library(sdn)
 source("calc-functions.R")
 palette("Okabe-Ito")
@@ -40,17 +41,21 @@ moranIs<- mcmapply(
     dfs, As, SIMPLIFY = FALSE, USE.NAMES = TRUE, mc.cores = ncores
 )
 ssds <- lapply(dfs, function(df) apply(df, 1, sd))
+vars <- lapply(dfs, function(df) apply(df, 1, moment, order = 2, central = TRUE))
+cvs <- lapply(dfs, function(df) apply(df, 1, CV))
 skews <- lapply(dfs, function(df) {
-    g_1 <- apply(df, 1, moments::skewness)
+    g_1 <- apply(df, 1, skewness)
                                         # Accounts for difference in expected change in skewness based on the
                                         # direction of the simulations:
     if(attr(df, "direction") == "down") g_1 <- -g_1
     g_1
 })
-kurts <- lapply(dfs, function(df) apply(df, 1, moments::kurtosis))
+kurts <- lapply(dfs, function(df) apply(df, 1, kurtosis))
 taus <- list(
     moranI = mapply(get_tau, dfs, moranIs, TRUE),
     ssd = mapply(get_tau, dfs, ssds, TRUE),
+    var = mapply(get_tau, dfs, vars, TRUE),
+    cv = mapply(get_tau, dfs, cvs, TRUE),
     skew = mapply(get_tau, dfs, skews, TRUE),
     kurt = mapply(get_tau, dfs, kurts, TRUE)
 )
